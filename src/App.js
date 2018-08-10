7115c76e66428bc83a96a8ef3df98b91c6d26f80
KSY <0419ksy@naver.com>
2018-08-10 17:21:50 +0900

    nasaImage api

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ViewerTemplate from './components/ViewerTemplate';
+import SpaceNavigator from './components/SpaceNavigator';
+
+class App extends Component {
+  render() {
+    return (
+        <ViewerTemplate
+          spaceNavigator={<SpaceNavigator/>}
+        />
+    );
+  }
+}
+
+export default App;
